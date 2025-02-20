b9323c4f844f4d85dab74b691028cd0416c59dd1
Ahmed Ashraf <ahmedashraf12777@gmail.com>
2025-02-20 20:01:00 +0200

    ziad-fix

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,13 +3,11 @@ import Banner from "./Banner";
 import Categories from "./Categories";
 import Hero from "./Hero";
 import TrendingProducts from "../shop/TrendingProducts";
-<<<<<<< HEAD
 import DealsSection from "./DealsSection";
 import PromoBanner from "./PromoBanner";
 import Blogs from "../blogs/Blogs";
 
-=======
->>>>>>> 56f12dcf997f851445817413c19312c95aef6080
+
 
 const Home = () => {
   return (
@@ -18,16 +16,11 @@ const Home = () => {
       <Categories />
       <Hero />
       <TrendingProducts />
-<<<<<<< HEAD
       <DealsSection />
       <PromoBanner />
       <Blogs />
     </div>
  );
-=======
-    </div>
-  );
->>>>>>> 56f12dcf997f851445817413c19312c95aef6080
 };
 
 export default Home;
